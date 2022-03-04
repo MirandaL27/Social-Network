496afaf9565d16d1e7b2c282edb6b65330c76f65
Cadenza <mirandalucsb@gmail.com>
2022-03-04 12:42:03 -0700

    added routes and controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,16 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    addThought({ params, body }, res) {
+    },
+    removeThought({ params }, res) {
+
+    },
+    addReaction({ params, body }, res) {
+    },
+    removeReaction({ params }, res) {
+
+    },
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+const userController = {
+
+    getAllUsers(req, res){
+
+    },
+    getUserById({ params }, res){
+
+    },
+    createUser({ body }, res){
+
+    },
+    updateUser({ params, body }, res){
+
+    },
+    deleteUser({ params }, res){
+        
+    }
+};
+
+module.exports = userController;
