519e5b937d5d23273005d605d1dbc11d98515cb6
Cadenza <mirandalucsb@gmail.com>
2022-03-09 17:56:07 -0700

    added friend routes to project

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -63,6 +63,35 @@ const userController = {
                 res.json(dbUserData);
             })
             .catch(error => res.status(400).json(error));
+    },
+
+    addFriend({params}, res){
+        User.findOneAndUpdate({_id: params.userId},
+            { $push: { friends: params.friendId } },
+            { new: true }            
+            )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(error => res.json(error));
+    },
+    removeFriend({params}, res){
+        User.findOneAndUpdate({_id: params.userId},
+            { $pull: { friends: params.friendId } },
+            { new: true }            
+            )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(error => res.json(error));
     }
 };
 
